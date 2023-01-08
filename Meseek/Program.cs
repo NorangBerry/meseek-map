275337b7a6a3dccf7727f5f4f1315a8fe2493c2d
NorangBerry <hlnsud12@naver.com>
2023-01-08 22:50:12 +0900

    initialize project

---
+++ Meseek/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Meseek;
+
+using Meseek.Crawler.BlueRibbon;
+using System.Runtime.CompilerServices;
+
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+    }
+
+    public static string GetThisFolderPath([CallerFilePath] string? path = null)
+        => Path.GetDirectoryName(path)!;
+}
